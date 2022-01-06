2a03a6344f736961f93d1686d16b3496bc31f034
Andrew Bidylo <bidyloandrew@gmail.com>
2022-01-06 20:47:32 +0000

    Connect the getAllReservations function to DB

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -1,5 +1,4 @@
 const properties = require('./json/properties.json');
-const users = require('./json/users.json');
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -15,8 +14,6 @@ pool.connect().then(() => {
   .catch((error) => {
     console.log('db connection error:', error);
   });
-
-
 
 /// Users
 
@@ -38,7 +35,7 @@ const getUserWithEmail = function (email) {
         return null;
       }
     })
-    .catch((err) => console.log('Error:', err));
+    .catch((err) => console.log('Error:', err.message));
 };
 
 exports.getUserWithEmail = getUserWithEmail;
@@ -48,7 +45,7 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
-const getUserWithId = function(id) {
+const getUserWithId = function (id) {
   return pool
     .query(`SELECT id, name, email, password
   FROM users
@@ -59,9 +56,9 @@ const getUserWithId = function(id) {
       } else {
         return null;
       }
-      
+
     })
-    .catch((err) => console.log('Error:', err));
+    .catch((err) => console.log('Error:', err.message));
 };
 exports.getUserWithId = getUserWithId;
 
@@ -71,11 +68,11 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function(user) {
+const addUser = function (user) {
   return pool
     .query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`, [user.name, user.email, user.password])
     .then((result) => result.rows[0])
-    .catch(() => null);
+    .catch((err) => console.log('Error:', err.message));
 };
 exports.addUser = addUser;
 
@@ -86,8 +83,20 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+const getAllReservations = function(guest_id, limit = 10) {
+  return pool
+    .query(`SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;`, [guest_id, limit])
+    .then((res) => res.rows)
+    .catch((err) => console.log('Error:', err.message));
+
 };
 exports.getAllReservations = getAllReservations;
 
